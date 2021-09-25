a95b9aacc39c10834fa52bcc2361b45bc1646700
Sriraj Bura <sriraj.bura16@gmail.com>
2021-09-25 11:07:24 -0400

    implemented post contorller

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -1,13 +1,34 @@
 const Link = require('../models/link');
+const { body, validationResult } = require('express-validator');
+const { nanoid } = require('nanoid');
 
 exports.get_link_form = (req, res, next) => {
   res.render('index');
 };
 
-exports.post_link_form = (req, res, next) => {
-  res.render('index');
-};
+exports.post_link_form = [
+  // url_validator,
+  body('url-link').trim().isURL().withMessage('URL invalid'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      res.render('index', { errors: errors.array() });
+      return;
+    } else {
+      const new_url = new Link({
+        link: req.body.link,
+        short_link,
+      });
+    }
+  },
+];
 
 exports.get_shorten_link = (req, res, next) => {
   res.render('index');
+  //get link by short id link
+  //redirect.
 };
+
+//validator
+//valid url
